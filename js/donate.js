69f12830a133c49cd7bc4f23b56f4e0ac68731cd
Tonvin Tian <itonvin@gmail.com>
2018-06-30 18:04:13 +0800

    fetch html only once

--- js/donate.js
+++ js/donate.js
@@ -4,13 +4,17 @@
 			$('.donate-author-post').each(function(){
                 $(this).show();
                 $(this).children('button').click(async function(){
-                    let url = '/wp-json/donate-author-post/page';
-                    let response = await fetch(url);
-                    let text = await response.text();
-                    $("body").append(text);
-                    $('.dap-close').click(function(){
-                        $('#donate-author-post__wrapper').remove();
-                    });
+                    if ( $('#donate-author-post__wrapper').length == 0 ) {
+                        let url = '/wp-json/donate-author-post/page';
+                        let response = await fetch(url);
+                        let text = await response.text();
+                        $("body").append(text);
+                        $('.dap-close').click(function(){
+                            $('#donate-author-post__wrapper').hide();
+                        });
+                    } else {
+                        $('#donate-author-post__wrapper').show();
+                    }
                 });
             });
 		}
